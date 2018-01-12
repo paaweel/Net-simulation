4a826552f377f28279d8b68b3dc8a55325a13ba7
pawel <pol4_1997@o2.pl>
2018-01-12 17:21:21 +0100

    Implemented functions for ReceiverPreferences (all but those with not uniform prob)

--- include/ReceiverPreferences.h
+++ include/ReceiverPreferences.h
@@ -5,17 +5,26 @@
 #ifndef NET_SIMULATION_RECEIVERPREFERENCES_H
 #define NET_SIMULATION_RECEIVERPREFERENCES_H
 
+#include <random>
+#include "Net_Simulation.h"
+
+
 class ReceiverPreferences {
 private:
     std::map<IPackageReceiver*, double> probabilities;
+    static std::mt19937 gen;
 public:
-    const std::map<IPackageReceiver*, double> getPreferences();
+    ReceiverPreferences();
+    std::map<IPackageReceiver*, double> getPreferences() {
+        return probabilities;
+    }
     void setPreferences(std::map<IPackageReceiver*, double>);
     void addReceiver(IPackageReceiver*);
     void addReceiverWithProbability(IPackageReceiver*, double);
     void removeReceiver(IPackageReceiver*);
-    IPackageReceiver* drawReceiver(double);
-    std::pair<IPackageReceiver*, double>* view();
+    IPackageReceiver* drawReceiver(); //parameter double is not required, yet
+    //returning vector, instead of an array
+    std::vector<std::pair<IPackageReceiver*, double>> view();
 }; // ReceiverPreferences
 
 
--- src/ReceiverPreferences.cpp
+++ src/ReceiverPreferences.cpp
@@ -2,4 +2,47 @@
 // Created by pawel on 12.01.2018.
 //
 
+#include <random>
 #include "../include/ReceiverPreferences.h"
+
+
+IPackageReceiver* ReceiverPreferences::drawReceiver() {
+    std::vector <double> val;
+    std::vector <IPackageReceiver*> key;
+    for (auto m : probabilities) {
+        val.push_back(m.second);
+        key.push_back(m.first);
+    }
+    std::discrete_distribution<int> d(val.begin(), val.end());
+
+    return key[d(gen)];
+}
+
+void ReceiverPreferences::addReceiver(IPackageReceiver*  newReceiver) {
+    probabilities[newReceiver] = 0;
+    for (auto & iterator : probabilities) {
+        iterator.second = 1.0/probabilities.size();
+    }
+}
+
+ReceiverPreferences::ReceiverPreferences() {
+    std::random_device rd;
+    std::mt19937 _gen(rd);
+    gen = _gen;
+}
+
+std::vector<std::pair<IPackageReceiver *, double>> ReceiverPreferences::view() {
+    std::vector<std::pair<IPackageReceiver *, double>> pairs;
+
+    for(const auto & p : probabilities) {
+        pairs.emplace_back(p);
+    }
+    return pairs;
+}
+
+void ReceiverPreferences::removeReceiver(IPackageReceiver * receiver) {
+    probabilities.erase(receiver);
+    for (auto & iterator : probabilities) {
+        iterator.second = 1.0/probabilities.size();
+    }
+}
