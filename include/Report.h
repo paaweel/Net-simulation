7cc22946bb7a2c30adc490137608131f172cf10f
Mateusz <mkwasny97@gmail.com>
2018-01-13 23:29:01 +0100

    First draft of ReportNotifier

---
+++ include/Report.h
@@ -0,0 +1,38 @@
+/*
+ * Report.h
+ *
+ *  Created on: 9 sty 2018
+ *      Author: Mateusz
+ */
+
+#ifndef REPORT_H_
+#define REPORT_H_
+
+#include"types.h"
+#include<set>
+
+class IReportNotifier
+{
+public:
+	virtual bool shouldGenerateReport(Time)=0;
+};
+
+class SpecificTurnsReportNotifier : public IReportNotifier
+{
+private:
+	std::set<Time> turns;
+public:
+	SpecificTurnsReportNotifier(std::set<Time>);
+	bool shouldGenerateReport(Time);
+};
+
+class IntervalReportNotifier : public IReportNotifier
+{
+private:
+	TimeOffset interval;
+public:
+	IntervalReportNotifier(TimeOffset);
+	bool shouldGenerateReport(Time);
+};
+
+#endif /* REPORT_H_ */
