77ab50853bdc2ec94d9db4a25b5ea445091a7fb4
pawel <pol4_1997@o2.pl>
2018-01-16 11:30:20 +0100

    Test for package receiver - removing a receiver

--- test/test_ReceiverPreferences.cpp
+++ test/test_ReceiverPreferences.cpp
@@ -75,3 +75,20 @@ TEST(ReceiverPreferences, addingWithProbability) {
     EXPECT_DOUBLE_EQ(0.7, prob[s0.get()]);
     EXPECT_DOUBLE_EQ(0.3, prob[s1.get()]);
 }
+
+TEST (ReceiverPreferences, removingReceiver) {
+    auto s0 = std::make_shared<StoreHouse> (0),
+            s1 = std::make_shared<StoreHouse> (1),
+            s2 = std::make_shared<StoreHouse> (2);
+
+    ReceiverPreferences rp;
+    std::map<IPackageReceiver*, double>  newPref = {{s0.get(), 0.2}, {s1.get(), 0.4}, {s2.get(),0.4}};
+    rp.setPreferences(newPref);
+
+    rp.removeReceiver(s0.get());
+
+    auto prob = rp.getPreferences();
+    EXPECT_DOUBLE_EQ(0.5, prob[s1.get()]);
+    EXPECT_DOUBLE_EQ(0.5, prob[s2.get()]);
+
+}
